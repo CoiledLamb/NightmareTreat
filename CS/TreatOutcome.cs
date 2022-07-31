13b5510781f0a5e4015b8ece5f996344bce69d8b
CoiledLamb <110322848+CoiledLamb@users.noreply.github.com>
2022-07-31 00:50:28 -0500

    Update TreatOutcome.cs

--- CS/TreatOutcome.cs
+++ CS/TreatOutcome.cs
@@ -1,4 +1,5 @@
 using System;
+
 namespace XRL.World.Parts
 {
     [Serializable]
@@ -6,27 +7,56 @@ namespace XRL.World.Parts
     {
         public static Effect CreateEffectByName(string name)
         {
-            return Activator.CreateInstance(ModManager.ResolveType("XRL.World.Effects." + name)) as Effect;
+            return Activator.CreateInstance(ModManager.ResolveType("XRL.World.Effects." + name))
+                as Effect;
         }
+
         public override void Register(GameObject Object)
         {
             Object.RegisterPartEvent(this, "OnEat");
             base.Register(Object);
         }
-            public override bool WantEvent(int ID, int cascade)
-    {
-        if (!base.WantEvent(ID, cascade) && ID != GetInventoryActionsAlwaysEvent.ID && ID != GetShortDescriptionEvent.ID)
+
+        public override bool WantEvent(int ID, int cascade)
         {
-            return ID == InventoryActionEvent.ID;
+            if (
+                !base.WantEvent(ID, cascade)
+                && ID != GetInventoryActionsAlwaysEvent.ID
+                && ID != GetShortDescriptionEvent.ID
+            )
+            {
+                return ID == InventoryActionEvent.ID;
+            }
+            return true;
         }
-        return true;
-    }
+
         public override bool HandleEvent(GetInventoryActionsAlwaysEvent E)
         {
-                E.AddAction("Eat", "eat", "Eat", null, 'e', FireOnActor: false, Default: 100, Priority: 0, Override: true);
-                E.AddAction("Feed To", "feed to", "Feed To", null, 'f', FireOnActor: false, Default: -1, Priority: 0, Override: true);   
-        return base.HandleEvent(E);    
+            E.AddAction(
+                "Eat",
+                "eat",
+                "Eat",
+                null,
+                'e',
+                FireOnActor: false,
+                Default: 100,
+                Priority: 0,
+                Override: true
+            );
+            E.AddAction(
+                "Feed To",
+                "feed to",
+                "Feed To",
+                null,
+                'f',
+                FireOnActor: false,
+                Default: -1,
+                Priority: 0,
+                Override: true
+            );
+            return base.HandleEvent(E);
         }
+
         public override bool FireEvent(Event E)
         {
             if (E.ID == "OnEat")
@@ -35,13 +65,13 @@ namespace XRL.World.Parts
                 var eater = E.GetGameObjectParameter("Eater");
                 var result = PopulationManager.RollOneFrom("TreatOutcome");
                 eater.PermuteRandomMutationBuys();
-            if (eater.HasEffect("GlotrotOnset"))
-            {
-                Popup.ShowFail(ParentObject.Does("seem") + "to taste like anything.");
-                E.eater.UseEnergy(1000, "Item Failure");
-                E.RequestInterfaceExit();
-                return false;
-            }
+                if (eater.HasEffect("GlotrotOnset"))
+                {
+                    Popup.ShowFail(ParentObject.Does("seem") + "to taste like anything.");
+                    E.eater.UseEnergy(1000, "Item Failure");
+                    E.RequestInterfaceExit();
+                    return false;
+                }
                 if (result.Hint == "Stat")
                 {
                     eater.GetStat(result.Blueprint).BaseValue += Bonus;
@@ -56,7 +86,4 @@ namespace XRL.World.Parts
             return base.FireEvent(E);
         }
     }
-
 }
-
-
